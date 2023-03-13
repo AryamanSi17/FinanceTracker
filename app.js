2d60dbd793b54a7c8765e09a518e27b6ff68491e
AryamanSi17 <106659803+AryamanSi17@users.noreply.github.com>
2023-03-14 01:33:47 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,62 +8,90 @@ const session=require("express-session");
 const app=express();
 app.use(bodyparser.urlencoded({extended:true}));
 const stuff = "this is test stuff for post";
-var datum = [""];
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs');
-// app.use(session({
-//     secret:"hello",
-//     resave:false,
-//     saveUninitialized:false
-//   }));
-//   app.use(passport.initialize());
-// app.use(passport.session());
+app.use(session({
+    secret:"hello",
+    resave:false,
+    saveUninitialized:false
+  }));
+  app.use(passport.initialize());
+app.use(passport.session());
+const userSchema=new mongoose.Schema ({
+    email:String,
+    password:String
+  });
+  userSchema.plugin(passportLocalMongoose);
 mongoose.connect("mongodb+srv://arydroid:06ewt0xm0T@cluster0.lody4r5.mongodb.net/userSDB",{useNewUrlParser:true});
 const itemSchema=new mongoose.Schema({
     amount:Number,
     item:String
 });
-// itemSchema.plugin(passportLocalMongoose);
+const User=new mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 const List=mongoose.model('List',itemSchema);
 app.get("/",function(req,res){
     res.render("home");
 });
+app.get("/login",function(req,res){
+    res.render("login");
+});
+app.post("/login",function(req,res){
+    const user=new User({
+        username:req.body.username,
+        password:req.body.password
+    });
+    req.login(user,function(err){
+        if(err){
+          console.log(err);
+        }
+        else{
+          passport.authenticate("local")(req,res,function(){
+            res.redirect("/data");
+          })
+        }
+      })
+})
 app.get("/register",function(req,res){
-    res.render("data");
+    res.render("register");
 });
 app.post("/register",function(req,res){
+    User.register({username:req.body.username},req.body.password,function(err,user){
+      if(err){
+        console.log(err);
+        res.redirect("/register");
+  
+      }
+      else{
+        passport.authenticate("local")(req,res,function(){
+          res.redirect("/data");
+        });
+      }
+    });
+  
+  });
+app.post("/data",function(req,res){
     const myData=new List({
         amount:req.body.num,
         item:req.body.entity
     });
     myData.save();
-        // if (err) {
-        //     console.error(err);
-        //     res.status(500).send('Error saving data to database');
-        //   } else {
-        //     alert("Data has been stored");
-            
-        //   }
-
-        //  if(!err){
-        // res.redirect("/");
-        //  }
-        // });
         res.redirect("/ana");
 });
 // app.get("/ana",function(req,res){
 //     res.render("anal")
 // });
-app.get("/ana",function(req,res){
-    List.find().then(function(posts){
-        // console.log(post);
-        
-    
-      res.render("ana" ,{posts:posts,data:stuff});
-    
-    });
-    });
-app.listen(5000,function(){
+app.get('/ana', async (req, res) => {
+    try {
+      const list = await List.find({});
+      res.render('ana', {list:list});
+    } catch (err) {
+      console.error(err);
+    }
+  });
+app.listen(3000,function(){
     console.log("Server started")
 });
